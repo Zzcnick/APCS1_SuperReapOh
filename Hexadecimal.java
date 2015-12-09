df7b9a2dd912deba5ca519ec6e97349ac265258e
Zzcnick <zzhen@stuy.edu>
2015-12-09 10:27:16 -0500

    Initial commit

---
+++ Hexadecimal.java
@@ -0,0 +1,266 @@
+/* Team Tofoo - Zicheng Zhen and Gabriel Marks
+   APCS1 pd10
+   HW44 -- This or That or Fourteen Other Things / Hexadecimal Class
+   2015-12-08 */
+
+public class Hexadecimal {
+
+    private final static String HEXDIGITS = "0123456789ABCDEF";
+    private int _decNum;
+    private String _hexNum;
+
+    /*=====================================
+      default constructor
+      pre:  n/a
+      post: initializes _decNum to 0, _hexNum to "0"
+      =====================================*/
+    public Hexadecimal() { 
+	_decNum = 0;
+	_hexNum = "0";
+    }
+
+
+    /*=====================================
+      overloaded constructor
+      pre:  n >= 0
+      post: sets _decNum to n, _hexNum to equiv string of bits
+      =====================================*/
+    public Hexadecimal( int n ) {
+	this();
+	_decNum = n;
+	_hexNum = decToHex(n);
+    }
+
+
+    /*=====================================
+      overloaded constructor
+      pre:  s is String representing non-negative hexadecimal number
+      post: sets _hexNum to input, _decNum to decimal equiv
+      =====================================*/
+    public Hexadecimal( String s ) {
+	this();
+	_decNum = hexToDec(s);
+	_hexNum = s;
+    }
+
+    /*=====================================
+      Accessors and Mutators
+      =====================================*/
+    public int getValue() {
+	return _decNum;
+    }
+
+    /*=====================================
+      String toString() -- returns String representation of this Object
+      pre:  n/a
+      post: returns String of 1's and 0's representing value of this Object
+      =====================================*/
+    public String toString() { 
+	return _hexNum;
+    }
+
+    /*=====================================
+      String chooseHex(int) - returns hexadec representation from 0 - 15
+      pre: 0 <= n < 16
+      post: returns String from 0 - 9, a - f 
+      =====================================*/
+    public static String chooseHex(int n) {
+	try {
+	    return HEXDIGITS.substring(n,n+1);
+	} catch (Exception ex) {
+	    System.out.println("ERROR: Index out of range.");
+	    return "";
+	}
+    }
+
+    /*=====================================
+      String chooseDec(String) - returns hexadec representation from 0 - 15
+      pre: "0" <= n < "f"
+      post: returns int from 0 to 15
+      =====================================*/
+    public static int chooseDec(String s) {
+	try {
+	    return HEXDIGITS.indexOf(s);
+	} catch (Exception ex) {
+	    System.out.println("ERROR: Index out of range.");
+	    return -1;
+	}
+    }
+	
+    /*=====================================
+      String decToHex(int) -- converts base-10 input to hexadecimal
+      pre:  n >= 0
+      post: returns String of bits
+      eg  decToHex(0) -> "0"
+      decToHex(1) -> "1"
+      decToHex(2) -> "10"
+      decToHex(3) -> "11"
+      decToHex(14) -> "1110"
+      =====================================*/
+    public static String decToHex( int n ) {
+	String hexVal = "";
+	while (n != 0) {
+	    int digit = n % 16;
+	    hexVal = chooseHex(digit) + hexVal;
+	    n /= 16;
+	}
+	if (hexVal.equals(""))
+	    return "0";
+	return hexVal;
+    }
+
+
+    /*=====================================
+      String decToHexR(int) -- converts base-10 input to hexadecimal, recursively
+      pre:  n >= 0
+      post: returns String of bits
+      eg  decToHexR(0) -> "0"
+      decToHexR(1) -> "1"
+      decToHexR(2) -> "10"
+      decToHexR(3) -> "11"
+      decToHexR(14) -> "1110"
+      =====================================*/
+    public static String decToHexR( int n ) {
+	if (n < 16) {
+	    return "" + chooseHex(n);
+	}
+	return decToHexR(n/16) + ("" + chooseHex(n%16));
+    }
+
+
+    /*=====================================
+      String hexToDec(String) -- converts base-10 input to hexadecimal
+      pre:  s represents non-negative hexadecimal number
+      post: returns decimal equivalent as int
+      eg  
+      hexToDec("0") -> 0
+      hexToDec("1") -> 1
+      hexToDec("10") -> 16
+      hexToDec("11") -> 17
+      hexToDec("AD") -> 173
+      =====================================*/
+    public static int hexToDec( String s ) {
+	int total = 0;
+	int times = 1;
+	for (int i = s.length()-1; i > -1; i--) {
+	    total += chooseDec(s.substring(i,i+1)) * times;
+	    times *= 16;
+	}
+	return total;
+    }
+
+    /*=====================================
+      String hexToDecR(String) -- converts base-10 input to hexadecimal, recursively
+      pre:  s represents non-negative hexadecimal number
+      post: returns decimal equivalent as int
+      eg  
+      hexToDecR("0") -> 0
+      hexToDecR("1") -> 1
+      hexToDecR("10") -> 16
+      hexToDecR("11") -> 17
+      =====================================*/
+    public static int hexToDecR( String s ) { 
+	if (s.length() == 1) {
+	    return chooseDec(s);
+	}
+	return 16 * hexToDecR(s.substring(0,s.length()-1)) +
+	    chooseDec(s.substring(s.length()-1));
+    }
+
+
+    /*=============================================
+      boolean equals(Object) -- tells whether 2 Objs are equivalent
+      pre:  other is an instance of class Hexadecimal
+      post: Returns true if this and other are aliases (pointers to same 
+      Object), or if this and other represent equal hexadecimal values
+      =============================================*/
+    public boolean equals( Object other ) { 
+	boolean retVal = this == other;
+	if (!(retVal)) {
+	    retVal = other instanceof Hexadecimal;
+	    if (retVal) {
+		return (this.compareTo(other) == 0);
+	    }
+	    return false;
+	}
+	return true;
+    }
+
+
+    /*=============================================
+      int compareTo(Object) -- tells which of two Hexadecimal objects is greater
+      pre:  other is instance of class Hexadecimal
+      post: Returns 0 if this Object is equal to the input Object,
+      negative integer if this<input, positive integer otherwise
+      =============================================*/
+    public int compareTo( Object other ) {
+	if (!(other instanceof Hexadecimal)) {
+	    throw new ClassCastException("Incorrect type of parameter");
+	}
+	if (_decNum > ((Hexadecimal)other).getValue()) {
+	    return 1;
+	} else if (_decNum < ((Hexadecimal)other).getValue()) {
+	    return -1;
+	} else {
+	    return 0;
+	}
+    }
+
+    //main method for testing
+    public static void main( String[] args ) {
+	System.out.println();
+	System.out.println( "Testing ..." );
+
+	Hexadecimal b1 = new Hexadecimal(5);
+	Hexadecimal b2 = new Hexadecimal("5");
+	Hexadecimal b3 = b1;
+	Hexadecimal b4 = new Hexadecimal(7);
+	Hexadecimal b5 = new Hexadecimal(266);
+	Hexadecimal b6 = new Hexadecimal("10A");
+	String s = "hallo";
+	
+	System.out.println( b1 );
+	System.out.println( b2 );
+	System.out.println( b3 );       
+	System.out.println( b4 );
+	System.out.println( b5 );
+	System.out.println( b6 );
+
+	System.out.println( "\nhexToDec" );
+	System.out.println(Hexadecimal.hexToDec("100")); //should be 256
+	System.out.println(Hexadecimal.hexToDec("DEF")); //should be 3567
+	System.out.println(Hexadecimal.hexToDec("ABE")); //should be 2750
+	
+	System.out.println( "\ndecToHex" );
+	System.out.println(Hexadecimal.decToHex(256)); //should be "100"
+	System.out.println(Hexadecimal.decToHex(3567)); //should be "3567"
+	System.out.println(Hexadecimal.decToHex(2750)); //should be "2750"
+
+	System.out.println( "\n==..." );
+	System.out.println( b1 == b2 ); //should be false
+	System.out.println( b1 == b3 ); //should be true
+
+	System.out.println( "\n.equals()..." );
+	System.out.println( b1.equals(b2) ); //should be true
+	System.out.println( b1.equals(b3) ); //should be true
+	System.out.println( b3.equals(b1) ); //should be true
+	System.out.println( b4.equals(b2) ); //should be false
+	System.out.println( b1.equals(b4) ); //should be false
+	System.out.println( b6.equals(b5) ); //should be true
+	System.out.println( b6.equals(s)  ); //should be false
+
+	System.out.println( "\n.compareTo..." );
+	System.out.println( b1.compareTo(b2) ); //should be 0
+	System.out.println( b1.compareTo(b3) ); //should be 0
+	System.out.println( b1.compareTo(b4) ); //should be neg
+	System.out.println( b4.compareTo(b1) ); //should be pos
+	try {
+	System.out.println( b5.compareTo(s)  ); //should throw ClassCastException
+	} catch (Exception e) {
+	    System.out.println(e);
+	}
+	/*=========================================
+	  =========================================*/
+    }//end main()
+
+} //end class
---
+++ SuperArray.java
@@ -0,0 +1,159 @@
+/* Team FeelTheBernieSanders - Zicheng Zhen and Matthew So
+   APCS1 pd10
+   HW42 -- Array of Titanium / More Implementation of Interfaces
+   2015-12-04 */
+
+public class SuperArray implements ListInt {
+
+    //~~~~~INSTANCE VARS~~~~~
+    //underlying container, or "core" of this data structure:
+    private int[] _data;
+
+    //position of last meaningful value
+    private int _lastPos;
+
+    //size of this instance of SuperArray
+    private int _size;
+
+		
+    //~~~~~METHODS~~~~~
+    //default constructor – initializes 10-item array
+    public SuperArray() {
+	_data = new int[10];
+	_lastPos = -1;
+	_size = 0;
+    }
+		
+    //output array in [a,b,c] format, eg
+    // {1,2,3}.toString() -> "[1,2,3]"
+    public String toString() {
+	String storage = "["; // Keeps track of non-zeros.
+	String stack = "";    // Keeps track of zeros.
+	for (int i = 0; i < _size; i++) {
+	    storage += _data[i] + ",";
+	}
+	if (storage.equals("[")) {return "[]";}
+	return storage.substring(0,storage.length()-1) + "]";
+    }
+
+    //swap - switches two indices of the array
+    //precond: int i1, i2 - two indices to be swapped
+    public void swap(int i1, int i2) {
+	_data[i1] = set(i2, _data[i1]);
+    }
+
+    //add - adds an item to the end of the array and gives the index meaning
+    //precond: int value - value to be added at the end
+    public void add(int value) {
+	if (_size+1 >= _data.length) {expand();} // Increases size if no space
+	_data[_lastPos+1] = value;
+	_lastPos += 1; // Afterwards, in case there is an error.
+	_size += 1;
+    }
+    //add - adds an item to an index of the array and shifts array to the right
+    //precond: int value - value to be added
+    //         int index - index to add to
+    public void add(int index, int value) {
+	if (_size+1 >= _data.length) {expand();}
+	add(value);
+	for (int i = _lastPos; i > index; i--) { // "bubble swap" backwards.
+	    swap(i,i-1);
+	}
+    }
+
+    //remove - removes an item at a specified index and shifts everything left
+    //precond: int index - index to be removed;
+    public void remove(int index) {
+	_data[index] = 0;
+	for (int i = index; i < _lastPos; i++) {
+	    swap(i,i+1);
+	}
+	_lastPos -= 1;
+	_size -= 1;
+    }
+    //remove - removes last meaningful element
+    public void remove() {
+	remove(_lastPos);
+    }
+    
+    //double capacity of this SuperArray
+    private void expand() {
+	int len = _data.length * 2;
+	int[] copy = new int[len];
+	for (int i = 0; i < len / 2; i++) {
+	    copy[i] = _data[i];
+	}
+	_data = copy;
+    }
+		
+    //accessor -- return value at specified index
+    public int get( int index ) {
+	return _data[index];
+    }
+    public int size() {
+	return _size;
+    }
+		
+    //mutator -- set value at index to newVal, 
+    //           return old value at index
+    public int set( int index, int newVal ) {
+	int storage = _data[index];
+	_data[index] = newVal;
+	return storage;
+    }
+
+
+    //main method for testing
+    public static void main( String[] args ) {
+	// ------PHASE I------
+	SuperArray oof = new SuperArray();
+	System.out.println("Phase I Testing:\n" + oof);
+	oof.expand();
+	System.out.println(oof);
+	for (int i = 0; i < 20; i++) {
+	    oof.set(i, i*i);
+	}
+	System.out.println(oof);
+	System.out.println(oof.get(3) + oof.get(4)); // 25
+	oof.expand();
+	// ------PHASE II------
+	System.out.println(oof + "\n\n" +
+			   "Phase II Testing:");
+	SuperArray rab = new SuperArray();
+	for (int i = 0; i < 10; i++) {
+	    rab.add(i);
+	}
+	System.out.println(rab);
+	rab.add(5,101);
+	System.out.println(rab);
+	for (int i = 11; i < 16; i++) {
+	    rab.add(i, i*10);
+	}
+	System.out.println(rab);
+	rab.remove(0);
+	System.out.println(rab);
+	rab.remove(4);
+	System.out.println(rab);
+	rab.remove();
+	rab.remove();
+	System.out.println(rab);
+	// ------PHASE III------
+	System.out.println(oof + "\n\n" +
+			   "Phase III Testing:");
+	ListInt foo = new SuperArray();
+	for (int i = 0; i < 10; i++) {
+	    foo.add(i*i*i);
+	}
+	foo.add(0,-10);
+	System.out.println(foo);
+	foo.add(100);
+	System.out.println(foo);
+	foo.remove(5);
+	System.out.println(foo);
+	System.out.println(foo.size());
+	System.out.println(foo.get(5));
+	foo.set(5,1000);
+	System.out.println(foo.get(5));
+    }//end main
+		
+}//end class
